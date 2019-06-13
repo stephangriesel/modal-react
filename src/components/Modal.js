b67d187fa166e7ee0247320bde381d6b68058d9a
Stephan <sgriesel@gmail.com>
2019-06-13 11:47:23 +0200

    add modal component

---
+++ src/components/Modal.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../css/Modal.css';
+
+class Modal extends Component {
+    render() {
+        return (
+            <div>
+                Hello Modal
+            </div>
+        )
+    }
+}
+
+export default Modal;
